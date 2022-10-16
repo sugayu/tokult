a257a108029111ae307d17b7a576ae515186249c
sugayu_i <sugayu@aoni.waseda.jp>
2022-10-16 16:16:19 +0900

    Minor change example

--- example/example.py
+++ example/example.py
@@ -2,18 +2,19 @@ import tokult
 import datetime
 import numpy as np
 from astropy.io import fits
+import multiprocessing
 
 tok = tokult.Tokult.launch(
     'cube_dirty.fits',
     'cube_dirty.psf.fits',
-    ('gamma1.fits', 'gamma2.fits', 'kappa.fits'),
+    ('x-arcsec-deflect.fits', 'y-arcsec-deflect.fits'),
 )
-tok.set_region((226, 286), (226, 286), (5, 12))
-tok.use_redshifts(z_lens=0.541, z_source=9.1111)
+tok.use_region((226, 286), (226, 286), (5, 12))
+tok.use_redshifts(z_source=9.1096, z_lens=0.541)
 
 hudl = fits.open('cube_dirty_uniform.psf.fits')
 uvpsf_uniform = tokult.misc.rfft2(np.squeeze(hudl[0].data))
-uvcoverage = (uvpsf_uniform[tok.datacube.vslice, :, :]) > 1.3e-5
+uvcoverage = (uvpsf_uniform[tok.datacube.vslice, :, :]) > 1.0e-5
 
 print(f'Start initialguess: {datetime.datetime.now()}')
 init = tok.initialguess()
@@ -24,12 +25,19 @@ bound = tokult.get_bound_params(
     PA_dyn=(0, 2 * np.pi),
     radius_dyn=(0.01, 5.0),
     velocity_sys=(5, 12),
-    mass_dyn=(-2.0, 3.0),
+    mass_dyn=(-5.0, 3.0),
     velocity_dispersion=(0.1, 3.0),
     brightness_center=(0.0, 1.0),
 )
 print(f'Start uvfit: {datetime.datetime.now()}')
-sol = tok.uvfit(
-    init=init, bound=bound, mask_for_fit=uvcoverage, nprocesses=4, progressbar=True
-)
+with multiprocessing.Pool(4) as pool:
+    sol = tok.uvfit(
+        init=init,
+        bound=bound,
+        mask_for_fit=uvcoverage,
+        pool=pool,
+        progressbar=True,
+        nwalkers=64,
+        nsteps=8000,
+    )
 print(f'End uvfit: {datetime.datetime.now()}')
